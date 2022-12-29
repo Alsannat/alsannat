5502c6d4b500d62dfde3f481976ab9dd8d389df7
Alsannat <moaz@alsannat.com>
2022-12-29 14:14:21 +0000

    Rating attribute mapping for google shopping feed

---
+++ app/code/Alsannat/RatingToProduct/Observer/RatingToProduct.php
@@ -0,0 +1,34 @@
+<?php
+namespace Alsannat\RatingToProduct\Observer;
+
+class RatingToProduct implements \Magento\Framework\Event\ObserverInterface
+{
+    public function __construct(
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        \Mageplaza\BetterProductReviews\Block\Review\Summary $reviewSummary
+    )
+    {
+        $this->productRepository = $productRepository;
+        $this->reviewSummary = $reviewSummary;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $reviewObject = $observer->getEvent()->getObject();
+        // $data = $object->getData();
+        // print_r($data); die;
+
+        if($reviewObject->getStatusId() == 1) // Approved case
+        {
+            $proId = (int)$reviewObject->getEntityPkValue();
+            $proObj = $this->productRepository->getById($proId);
+
+            $_ratingSummary = $this->reviewSummary->getRatingSummary($proObj);
+            $rating = number_format((float)($_ratingSummary / 20), 1);
+            
+            $proObj->setFeedRating($rating);
+            $this->productRepository->save($proObj);   
+        }
+        
+    }
+}
---
+++ app/code/Alsannat/RatingToProduct/registration.php
@@ -0,0 +1,6 @@
+<?php
+Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Alsannat_RatingToProduct',
+    __DIR__
+);
